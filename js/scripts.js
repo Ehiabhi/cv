5adb2f387de5800db4ec6748bea07fc1a71122be
Ehizy <50112787+Ehiabhi@users.noreply.github.com>
2020-04-22 10:18:57 +0100

    Add new updates
    Extra javascript functionality, modified html and css files

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@ const year = new Date().getFullYear();
 
 document.getElementById("footer").innerHTML =
   "© " + year + " All rights reserved.";
+
+$(document).ready(
+  $(document).scroll(function () {
+    var scrollPosition = $(this).scrollTop();
+    if (scrollPosition > 20) {
+      $("header").addClass("header-scroll-effect");
+      $(".social-nav").addClass("nav-item");
+    } else {
+      $("header").removeClass("header-scroll-effect");
+      $(".social-nav").removeClass("nav-item");
+    }
+  })
+);
